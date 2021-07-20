80e0c7b065bce7ae7d80ad7f98ac50efc0ebcee4
JiWon Lee <jiwon.swdev@gmail.com>
2021-07-20 16:11:33 +0900

    feat: configure base tableview in write scene

--- DaangnClone/ViewController/WriteViewController.swift
+++ DaangnClone/ViewController/WriteViewController.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
 
 class WriteViewController: BaseViewController {
+    // MARK: - Properties
+    private let disposeBag: DisposeBag = DisposeBag()
+
+    // MARK: - View Properties
+    private let tableView: UITableView = UITableView(frame: .zero, style: .grouped).then {
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "test")
+        $0.backgroundColor = .systemBackground
+        $0.sectionHeaderHeight = 0
+        $0.sectionFooterHeight = 0
+        $0.tableHeaderView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 0, height: CGFloat.leastNormalMagnitude)))
+        $0.tableFooterView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 0, height: CGFloat.leastNormalMagnitude)))
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigation()
         configureToolbar()
+        configureTableView()
+        bindTableView()
     }
 }
 
@@ -46,5 +65,23 @@ extension WriteViewController {
         let frequentlyUsedPhrasesBarButtonItem: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "text.badge.plus"), style: .plain, target: self, action: nil)
         let hideKeyboardBarButtonItem: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "keyboard.chevron.compact.down"), style: .plain, target: self, action: nil)
         toolbar.setItems([neighborhoodRangeBarButtonItem, flexibleSpace, frequentlyUsedPhrasesBarButtonItem, hideKeyboardBarButtonItem], animated: true)
+    }
+
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leadingMargin)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailingMargin)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+
+    private func bindTableView() {
+        Observable.just(["scroll", "title", "category", "price", "contents"])
+            .bind(to: tableView.rx.items(cellIdentifier: "test", cellType: UITableViewCell.self)) { row, element, cell in
+                cell.textLabel?.text = element
+            }
+            .disposed(by: disposeBag)
     }
 }
