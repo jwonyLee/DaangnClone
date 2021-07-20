6f790ae8e0b4dc791ac9d8ca308b961aa34eaa8a
JiWon Lee <jiwon.swdev@gmail.com>
2021-07-20 15:36:29 +0900

    feat: configure toolbar in write scene

--- DaangnClone/ViewController/WriteViewController.swift
+++ DaangnClone/ViewController/WriteViewController.swift
@@ -12,6 +12,7 @@ class WriteViewController: BaseViewController {
         super.viewDidLoad()
 
         configureNavigation()
+        configureToolbar()
     }
 }
 
@@ -24,7 +25,26 @@ extension WriteViewController {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: nil)
     }
 
-    @objc private func cancleButtonTapped() {
+    @objc
+    private func cancleButtonTapped() {
         self.dismiss(animated: true, completion: nil)
+    }
+
+    private func configureToolbar() {
+        let toolbar: UIToolbar = UIToolbar(frame: CGRect(origin: .zero, size: CGSize(width: view.frame.width, height: Styles.grid(11))))
+        toolbar.sizeToFit()
+        toolbar.tintColor = .label
+        view.addSubview(toolbar)
+        toolbar.snp.makeConstraints {
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leadingMargin)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailingMargin)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottomMargin)
+        }
+
+        let flexibleSpace: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let neighborhoodRangeBarButtonItem: UIBarButtonItem = UIBarButtonItem(title: "당근동과 근처 동네 3개", style: .plain, target: self, action: nil)
+        let frequentlyUsedPhrasesBarButtonItem: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "text.badge.plus"), style: .plain, target: self, action: nil)
+        let hideKeyboardBarButtonItem: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "keyboard.chevron.compact.down"), style: .plain, target: self, action: nil)
+        toolbar.setItems([neighborhoodRangeBarButtonItem, flexibleSpace, frequentlyUsedPhrasesBarButtonItem, hideKeyboardBarButtonItem], animated: true)
     }
 }
